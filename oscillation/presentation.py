428bea4677642994a73cc063df9d5018ff377ffc
ErbB4 <miab736621299@gmail.com>
2017-02-24 22:09:43 +0100

    tACS

--- oscillation/presentation.py
+++ oscillation/presentation.py
@@ -1,2 +1,6 @@
-# alpha frequency tACS increases endogeneous alpha oscillation
+# EEG evidence: alpha frequency tACS increases endogeneous alpha oscillation
 
+![alt text](https://github.com/ErbB4/neuroscience-notebook/blob/master/oscillation/tACS.png)
+
+
+# network simulation: 
